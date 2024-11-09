eacb83b7877be00b7416f0a12ced23c29963a356
Durai29 <muruga6129@gmail.com>
2024-11-09 21:59:51 +0530

    Just added new files

--- Exp4/_4Four.java
+++ Exp4/_4Four.java
@@ -1,4 +1,11 @@
 package Exp4;
+class Library{
+    Book book1 = new Book("R.S.Aggarwal", "The complete reference", 1000,"Schildt",10);
+    Book book2 = new Book("Dan Brown", "Inferno", 700, "Corgi",5);
+    Book book3 = new Book("J.K.Rowling", "Harry Potter", 500, "BloomsBurry", 1);
+
+   
+}
 
 class Book{
     String author;
@@ -7,15 +14,17 @@ class Book{
     String publisher;
     static int stock;
 
-    public Book(String author, String title, double price, String publisher){
+    public Book(String author, String title, double price, String publisher,int stock){
         this.author = author;
         this.title = title;
         this.price = price;
         this.publisher = publisher;
+        Book.stock = stock;
     }
 }
+
 public class _4Four {
     public static void main(String arg[]){
-        
+
     }
 }
