52a37c764806670bda2c1b24fb1a4ba8641229a7
Durai <muruga6129@gmail.com>
2024-11-09 21:17:23 +0530

    4 added

---
+++ Exp4/_4Four.java
@@ -0,0 +1,21 @@
+package Exp4;
+
+class Book{
+    String author;
+    String title;
+    double price;
+    String publisher;
+    static int stock;
+
+    public Book(String author, String title, double price, String publisher){
+        this.author = author;
+        this.title = title;
+        this.price = price;
+        this.publisher = publisher;
+    }
+}
+public class _4Four {
+    public static void main(String arg[]){
+        
+    }
+}
